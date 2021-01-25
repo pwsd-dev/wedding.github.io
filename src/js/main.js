aaccc250c4510a4c895745bac628fe331efda84f
nikitashvetsov21 <62443471+nikitashvetsov21@users.noreply.github.com>
2021-01-25 09:20:24 -0800

    add

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+if(document.getElementById('projectsSlider')){
+    var slider = tns({
+        container: '#projectsSlider',
+        items: 1,
+        slideBy: 'page',
+        autoplay: false,
+        prevButton: '.projects-btn_prev',
+        nextButton: '.projects-btn_next',
+        nav: false,
+        loop: true
+      });
+}
+  
+//дальше копирую полностью конструкцию, вместе с циклом if
+
+
